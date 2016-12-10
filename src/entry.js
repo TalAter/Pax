1fbc7ae0bb71afaea2d117d732ad7fe467436536
Tal Ater <tal@talater.com>
2016-12-10 16:46:44 +0100

    Removed service worker from basic script

--- src/entry.js
+++
@@ -1,9 +0,0 @@
-var swURL = require('file-loader!./serviceworker.js');
-
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register(swURL).then(registration =>
-    console.log('Service Worker registered successfully with scope: ', registration.scope)
-  ).catch(err =>
-    console.log('Service Worker registration failed: ', err)
-  );
-}
