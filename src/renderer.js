df8ad61d99b3988d37da9a9fea2c036cef999c3c
Tal Ater <tal@talater.com>
2016-12-24 00:16:57 +0200

    Tweaked some renderer settings

--- src/renderer.js
+++ src/renderer.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import Terrain from './terrain';
 
 var scene, camera, renderer;
 
@@ -8,22 +9,22 @@ const init = function() {
 
   scene = new THREE.Scene();
 
-  camera = new THREE.OrthographicCamera( width / - 2, width / 2, height / 2, height / - 2, 0.1, 1000 );
+  camera = new THREE.OrthographicCamera(width / - 2, width / 2, height / 2, height / - 2, 0.1, 1000);
   camera.position.set(200, 200, 200);
-  camera.lookAt( scene.position );
+  camera.lookAt(scene.position);
 
   renderer = new THREE.WebGLRenderer();
-  renderer.setSize( width, height );
+  renderer.setSize(width, height);
 
-  let ambientLight = new THREE.AmbientLight( 0xaaaaaa );
+  let ambientLight = new THREE.AmbientLight(0x999999);
   scene.add(ambientLight);
 
-  let directionalLight = new THREE.DirectionalLight(0xffffff);
-  directionalLight.position.set(-0.3, 0.7, 0.6);
+  let directionalLight = new THREE.DirectionalLight(0x777777);
+  directionalLight.position.set(3, 7, 6);
   directionalLight.position.normalize();
   scene.add(directionalLight);
 
-  document.body.appendChild( renderer.domElement );
+  document.body.appendChild(renderer.domElement);
 };
 
 const render = function() {
@@ -31,7 +32,7 @@ const render = function() {
 };
 
 const setBackgroundColor = function(color) {
-  scene.background = new THREE.Color( color );
+  scene.background = new THREE.Color(color);
 };
 
 /**
