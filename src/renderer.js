ccc00ed64e1f70fdb50b3579afa6c65a11cfb4cc
Tal Ater <tal@talater.com>
2016-12-24 00:28:05 +0200

    Render some random terrain to the view

--- src/renderer.js
+++ src/renderer.js
@@ -10,11 +10,33 @@ const init = function() {
   scene = new THREE.Scene();
 
   camera = new THREE.OrthographicCamera(width / - 2, width / 2, height / 2, height / - 2, 0.1, 1000);
-  camera.position.set(200, 200, 200);
+  camera.position.set(200, 300, 200);
   camera.lookAt(scene.position);
 
   renderer = new THREE.WebGLRenderer();
   renderer.setSize(width, height);
+
+  // Create some random terrain
+  const maxX = 100;
+  const maxY = 100;
+  let heightMap = Terrain.generate(maxX, maxY);
+  heightMap[1][1] = 0.2;
+
+  let geometry = new THREE.PlaneGeometry(2500, 2500, maxX-1, maxY-1);
+  geometry.rotateX(-Math.PI / 2);
+
+  let vertices = geometry.vertices;
+  for (let x = 0; x < maxX; x++) {
+    for (let y = 0; y < maxY; y++) {
+      vertices[x + y * maxY].y = heightMap[x][y]*30;
+    }
+  }
+  geometry.computeFlatVertexNormals();
+  // geometry.computeVertexNormals();
+
+  let material = new THREE.MeshLambertMaterial({ color: 0xdd8888, shading: THREE.SmoothShading });
+  let mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
 
   let ambientLight = new THREE.AmbientLight(0x999999);
   scene.add(ambientLight);
